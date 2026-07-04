3c2362347d7a1bbb31fc652d81d127c95449972b
Codenchai07 <codenchai07@gmail.com>
2026-07-04 16:44:16 +0530

    selection sort

--- Arrays/selectionSort.py
+++ Arrays/selectionSort.py
@@ -2,14 +2,14 @@ arr = [4, 3, 10, 2, 8, 9]
 
 def selectionSortAlgo(arr):
     n = len(arr)
-    
-    for i in range(n-1):
+    for i in range(n - 1):
         mini = i
-        for j in range(i+1, n):
+        for j in range (i+1, n):
             if(arr[j] < arr[mini]):
                 mini = j
-                arr[j], arr[mini] = arr[mini], arr[j]
-                
+            
+        arr[i], arr[mini] = arr[mini], arr[i]
 
+print("before sorted: ", arr)
 selectionSortAlgo(arr)
-print(arr)
+print("after sorted: ", arr)
