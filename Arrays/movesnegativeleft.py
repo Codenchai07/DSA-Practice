88f946d105c981ed9aa0ecee2443cc2c2d3acd90
Codenchai07 <codenchai07@gmail.com>
2026-08-02 19:39:54 +0530

    move all negative numbers at the started of the array or left of the array

---
+++ Arrays/movesnegativeleft.py
@@ -0,0 +1,28 @@
+arr = [5, -8, -2, 9, 1, -4]
+
+
+def move_negative_numbers_left(arr):
+    """This function solve the classic array problem that placed the all negative number at start or left"""
+    i = 0
+    j = 0
+    
+    n = len(arr)
+    
+    while i <= n - 1:
+        if arr[i] < 0:
+            arr[j], arr[i] = arr[i], arr[j]
+            i += 1
+            j += 1
+        else:
+            i += 1
+            
+    display_array(arr)
+    
+
+def display_array(arr):
+    print(arr)
+    
+    
+    
+move_negative_numbers_left(arr)
+        
